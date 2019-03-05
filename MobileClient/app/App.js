0e0ac39c476916fd5fac2215011a6b9f5d39e22b
rgabeflores <rgabeflores@gmail.com>
2019-03-05 01:43:16 -0800

    added redux

--- MobileClient/app/App.js
+++ MobileClient/app/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import { Provider } from "react-redux";
+import store from './redux/store';
+
+import styles from './styles';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>IoT Controller</Text>
+          <Toggler
+            name={"LED Strip"}
+            isOn={false}
+          />
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
